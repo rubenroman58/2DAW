3deeba6870390bd890b9b2d1aab9af2253d37461
rubenroman58 <ruben.malaga22@gmail.com>
2025-02-12 21:14:19 +0100

    ed

--- UnoDjango/core/views.py
+++ UnoDjango/core/views.py
@@ -1,26 +1,40 @@
-from django.shortcuts import render
-#importamos los modelos a utilizar
+from django.shortcuts import render, redirect
+# importamos los modelos a utilizar
 from core.models import Author
 from core.models import Book
-from core.forms import AutorForm 
+from core.forms import AutorForm
 
 
-#def home (request):
-##   autor=Author.objects.all() 
+# def home (request):
+# autor=Author.objects.all()
 
- #   return render(request,'core/index.html',{'authors':autor}) 
-def home (request):
+#   return render(request,'core/index.html',{'authors':autor})
+def home(request):
 
-    #return render(request,'index.html',{'authors':autor}) 
-    return render(request,'core/index.html',{'books':Book})
+    # return render(request,'index.html',{'authors':autor})
+    return render(request, 'core/index.html', {'books': Book})
 
 
-def autor_list (request):
-    autor=Author.objects.all()
-    return render(request,'core/autor.html',{'authors':autor})
-def libro_list (request):
-    libro=Book.objects.all()
-    return render(request,'core/libro.html',{'books':libro})
+def autor_list(request):
+    autor = Author.objects.all()
+    return render(request, 'core/autor.html', {'authors': autor})
 
-def autor_create (request):  
- return render(request, 'core/create_autor.html', {'autor_form': AutorForm}) #Si tenemos las páginas dentro de alguna  carpeta dentro de template, entonces habría que especificar el nombre de la carpeta Ej:  return render(request, 'nombre_carpeta/create_autor.html') 
+
+def libro_list(request):
+    libro = Book.objects.all()
+    return render(request, 'core/libro.html', {'books': libro})
+
+
+def autor_create(request):
+    if request.method == 'GET':
+        return render(request, 'core/create_autor.html', {'autor_form': AutorForm})
+    
+    if request.method == 'POST':
+        form=AutorForm(data=request.POST)
+    if form.is_valid:
+        form.save() 
+        print('tttttt')
+        return redirect ('/autor/')
+    else:
+        form=AutorForm(data=request.POST)
+        return render (request, 'core/create_autor.html',{'autor_form': AutorForm})
